93beb0e8ae6ca92ade659c782fc2102f612dda57
Akhil Potla <akhil4400@gmail.com>
2026-08-08 15:15:21 -0500

    Cache ttl as a variable

--- app/cache.py
+++ app/cache.py
@@ -1,8 +1,10 @@
 from app.redis import r
 
+cache_ttl = 5 * 60
+
 
 def cache_url(url: str, metadata: dict):
-    r.set(url, metadata, ex=60)
+    r.set(url, metadata, ex=cache_ttl)
 
 
 def get_cached_url(url: str):
