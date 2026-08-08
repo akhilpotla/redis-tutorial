f241a262284da10abc004f42c2a44482a1fc4c9c
Akhil Potla <akhil4400@gmail.com>
2026-08-08 15:00:56 -0500

    Connecting to redis container

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 import uvicorn
 from fastapi import FastAPI, status
+import redis
+
+from app.redis import r
 
 app = FastAPI()
 
@@ -12,6 +15,16 @@ async def root():
 @app.get("/health", status_code=status.HTTP_200_OK)
 async def health_check():
     return {"status": "healthy"}
+
+
+@app.get("/redis", status_code=status.HTTP_200_OK)
+async def redis_check():
+    try:
+        r.ping()
+        return {"status": "Redis is running!"}
+    except redis.exceptions.ConnectionError as e:
+        print("Error occurred while pinging Redis:", e)
+        return {"status": "Redis is not running!"}
 
 
 if __name__ == "__main__":
