e8ace18e500229fcf790a499bc2495bbd481f70d
Akhil Potla <akhil4400@gmail.com>
2026-08-13 18:44:43 -0500

    Storing job state

--- app/queue.py
+++ app/queue.py
@@ -1,4 +1,5 @@
 import uuid
+from datetime import datetime
 
 from app.redis import r
 
@@ -8,6 +9,11 @@ queue_name = "jobs"
 async def enqueue_url(url: str):
     job_id = uuid.UUID(url)
     r.lpush(queue_name, {"url": url, "job_id": job_id})
+    key_name = f"job:{job_id}"
+    created_at = datetime.now().timestamp()
+    r.hset(key_name, "status", "queue")
+    r.hset(key_name, "url", url)
+    r.hset(key_name, "created_at", created_at)
     return job_id
 
 
