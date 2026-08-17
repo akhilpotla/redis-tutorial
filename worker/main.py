9cbe02c7a84870107b43e3299caffc67c63109a0
Akhil Potla <akhil4400@gmail.com>
2026-08-16 21:44:52 -0500

    Error handling for worker

--- worker/main.py
+++ worker/main.py
@@ -2,6 +2,10 @@ import redis
 
 r = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
 
-while True:
-    _, job = r.brpop("jobs")
-    print(f"Job: {job}")
+try:
+    r.ping()
+    while True:
+        _, job = r.brpop("jobs")
+        print(f"Job: {job}")
+except Exception as e:
+    print(f"Error: {e}")
